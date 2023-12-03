4ddb006e91683d5e956cecf8a56354a32117ab1c
Frank Li <li.26.frank@gmail.com>
2023-12-02 20:34:43 -0500

    2023 day 2 part 2

--- 2023/day2/cube_conundrum.py
+++ 2023/day2/cube_conundrum.py
@@ -26,6 +26,30 @@
 
 # Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 
+# --- Part Two ---
+
+# The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+# As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+# Again consider the example games from earlier:
+
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+# Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+# Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+# Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+# In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+# Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+# Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+# Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+# Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+# The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+# For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+
 
 import sys
 
@@ -69,9 +93,34 @@ def part1(games):
         if is_possible_game(game, cube_set):
             id_sum += i + 1
 
-    return id_sum        
+    return id_sum
+
+def get_min_cube_set(game):
+    cube_set = { "red": 0, "green": 0, "blue": 0 }
+    for round in game:
+        for color, num_cubes in round.items():
+            cube_set[color] = max(num_cubes, cube_set[color])
+
+    return cube_set
+
+def calculate_cube_set_power(cube_set):
+    power = 1
+    for num_cubes in cube_set.values():
+        power *= num_cubes
+
+    return power
+
+def part2(games):
+    cube_set_power_sum = 0
+    for game in games:
+        min_cube_set = get_min_cube_set(game)
+        cube_set_power = calculate_cube_set_power(min_cube_set)
+        cube_set_power_sum += cube_set_power
+
+    return cube_set_power_sum
 
 if __name__ == "__main__":
     games = get_games()
     
     print(f'part 1: {part1(games)}')
+    print(f'part 2: {part2(games)}')
