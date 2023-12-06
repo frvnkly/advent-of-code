8059ffa4b106c6f99bf42f2a895e73e0dc5d64a2
Frank Li <li.26.frank@gmail.com>
2023-12-05 22:17:43 -0500

    2023 day 5 part 2

--- 2023/day5/if_you_give_a_seed_a_fertilizer.py
+++ 2023/day5/if_you_give_a_seed_a_fertilizer.py
@@ -99,6 +99,22 @@
 
 # What is the lowest location number that corresponds to any of the initial seed numbers?
 
+# --- Part Two ---
+
+# Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+# The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+# seeds: 79 14 55 13
+
+# This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+# Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+# In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+# Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+
 
 import sys
 
@@ -187,7 +203,23 @@ def part1(seeds, conversion_maps):
 
     return min(location_ranges)[0]
 
+def part2(seeds, conversion_maps):
+    seed_ranges = []
+    for i in range(0, len(seeds), 2):
+        start = seeds[i]
+        size = seeds[i+1]
+        seed_ranges.append([start, size])
+
+    location_ranges = []
+    for seed_range in seed_ranges:
+        location_ranges.extend(
+            get_seed_location_ranges(seed_range, conversion_maps)
+        )
+
+    return min(location_ranges)[0]
+
 if __name__ == "__main__":
     seeds, conversion_maps = get_almanac()
 
     print(f"part 1: {part1(seeds, conversion_maps)}")
+    print(f"part 2: {part2(seeds, conversion_maps)}")
