7e779895ac2def2e5bf01857a9960167b0558b62
Frank Li <li.26.frank@gmail.com>
2023-12-09 01:50:03 -0500

    2023 day 8 part 2

--- 2023/day8/haunted_wasteland.py
+++ 2023/day8/haunted_wasteland.py
@@ -32,11 +32,47 @@
 
 # Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 
+# --- Part Two ---
 
+# The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+
+# What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime? Only one way to find out.
+
+# After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+
+# For example:
+
+# LR
+
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)
+
+# Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+# Step 0: You are at 11A and 22A.
+# Step 1: You choose all of the left paths, leading you to 11B and 22B.
+# Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+# Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+# Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+# Step 5: You choose all of the left paths, leading you to 11B and 22C.
+# Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+# So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+# Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+
+
+from functools import reduce
 import sys
 
 sys.path.append(".")
-from util import get_input
+from util import get_input, lcm
 
 def get_directions_and_network():
     lines = get_input("2023/day8/input.txt")
@@ -50,20 +86,33 @@ def get_directions_and_network():
 
     return directions, network
 
-def part1(directions, network):    
-    current = "RPA"
-    steps = 0
-    while current != "ZZZ":
-        if current[-1] == "Z": print(current)
-        direction = directions[steps % len(directions)]
+def get_cycle_length(directions, network, start):
+    current = start
+    cycle_length = 0
+    while current[-1] != "Z":
+        direction = directions[cycle_length % len(directions)]
         i = 0 if direction == "L" else 1
 
         current = network[current][i]
-        steps += 1
-    
+        cycle_length += 1
+
+    return cycle_length
+
+def part1(directions, network):        
+    return get_cycle_length(directions, network, "AAA")
+
+def part2(directions, network):
+    starts = filter(lambda x: x[-1] == "A", network.keys())
+    paths = list(map(
+        lambda x: get_cycle_length(directions, network, x),
+        starts)
+    )
+
+    steps = reduce(lambda a, b: lcm(a, b), paths)
     return steps
 
 if __name__ == "__main__":    
     directions, network = get_directions_and_network()
 
     print(f"part 1: {part1(directions, network)}")
+    print(f"part 2: {part2(directions, network)}")
--- util.py
+++ util.py
@@ -1,6 +1,20 @@
+def gcd(a, b):
+    dividend = max(a, b)
+    divisor = min(a, b)
+    while True:
+        remainder = dividend % divisor
+
+        if not remainder: return divisor
+
+        dividend = divisor
+        divisor = remainder
+
 def get_input(filename):
     lines = []
     with open(filename, 'r') as file:
         for line in file: lines.append(line.strip())
         file.close()
     return lines
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
