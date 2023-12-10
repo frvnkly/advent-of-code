bb2b6b898cb70febe8e4a3a4e91cbc51d72dc160
Frank Li <li.26.frank@gmail.com>
2023-12-09 23:03:55 -0500

    2023 day 9 part 2

--- 2023/day9/mirage_maintenance.py
+++ 2023/day9/mirage_maintenance.py
@@ -72,6 +72,26 @@
 
 # Analyze your OASIS report and extrapolate the next value for each history. What is the sum of these extrapolated values?
 
+# --- Part Two ---
+
+# Of course, it would be nice to have even more history included in your report. Surely it's safe to just extrapolate backwards as well, right?
+
+# For each history, repeat the process of finding differences until the sequence of differences is entirely zero. Then, rather than adding a zero to the end and filling in the next values of each previous sequence, you should instead add a zero to the beginning of your sequence of zeroes, then fill in new first values for each previous sequence.
+
+# In particular, here is what the third example history looks like when extrapolating back in time:
+
+# 5  10  13  16  21  30  45
+#   5   3   3   5   9  15
+#    -2   0   2   4   6
+#       2   2   2   2
+#         0   0   0
+
+# Adding the new values on the left side of each sequence from bottom to top eventually reveals the new left-most history value: 5.
+
+# Doing this for the remaining example data above results in previous values of -3 for the first history and 0 for the second history. Adding all three new values together produces 2.
+
+# Analyze your OASIS report again, this time extrapolating the previous value for each history. What is the sum of these extrapolated values?
+
 
 import sys
 
@@ -104,14 +124,21 @@ def predict_value(history):
 
     return addend
 
-def part1(report):
+def sum_extrapolated_values(report):
     extrapolated_values_sum = 0
     for history in report:
         extrapolated_values_sum += predict_value(history)
 
     return extrapolated_values_sum
 
+def part1(report):    
+    return sum_extrapolated_values(report)
+
+def part2(report):
+    return sum_extrapolated_values(list(map(lambda x: x[::-1], report)))
+
 if __name__ == "__main__":
     report = get_report()
 
     print(f"part 1: {part1(report)}")
+    print(f"part 2: {part2(report)}")
