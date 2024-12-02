4112cad136aa64b12ed2506666c8da9371a484c6
Frank Li <li.26.frank@gmail.com>
2024-12-02 16:11:19 -0500

    2024 day 2 part 2

--- 2024/day2/red-nosed-reports.py
+++ 2024/day2/red-nosed-reports.py
@@ -35,6 +35,26 @@
 
 # Analyze the unusual data from the engineers. How many reports are safe?
 
+# --- Part Two ---
+
+# The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+# The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+# Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+# More of the above example's reports are now safe:
+
+# 7 6 4 2 1: Safe without removing any level.
+# 1 2 7 8 9: Unsafe regardless of which level is removed.
+# 9 7 6 2 1: Unsafe regardless of which level is removed.
+# 1 3 2 4 5: Safe by removing the second level, 3.
+# 8 6 4 4 1: Safe by removing the third level, 4.
+# 1 3 6 7 9: Safe without removing any level.
+
+# Thanks to the Problem Dampener, 4 reports are actually safe!
+
+# Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
 
 import sys
 
@@ -65,6 +85,20 @@ def part1(reports):
     
     print(f'part 1: {num_safe}')
 
+def part2(reports):
+    num_safe = 0
+    for report in reports:
+        if is_safe(report):
+            num_safe += 1
+        else:
+            for i in range(len(report)):
+                if is_safe(report[:i] + report[i+1:]):
+                    num_safe += 1
+                    break
+    
+    print(f'part 2: {num_safe}')
+
 if __name__ == '__main__':
     reports = get_reports()
     part1(reports)
+    part2(reports)
