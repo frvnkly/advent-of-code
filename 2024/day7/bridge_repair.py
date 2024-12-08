1942898ca2519df03cb2241d8c7bacf71aea8952
Frank Li <li.26.frank@gmail.com>
2024-12-08 04:19:46 -0500

    2024 day 7 part 2

--- 2024/day7/bridge_repair.py
+++ 2024/day7/bridge_repair.py
@@ -32,6 +32,22 @@
 
 # Determine which equations could possibly be true. What is their total calibration result?
 
+# --- Part Two ---
+
+# The engineers seem concerned; the total calibration result you gave them is nowhere close to being within safety tolerances. Just then, you spot your mistake: some well-hidden elephants are holding a third type of operator.
+
+# The concatenation operator (||) combines the digits from its left and right inputs into a single number. For example, 12 || 345 would become 12345. All operators are still evaluated left-to-right.
+
+# Now, apart from the three equations that could be made true using only addition and multiplication, the above example has three more equations that can be made true by inserting operators:
+
+# 156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
+# 7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
+# 192: 17 8 14 can be made true using 17 || 8 + 14.
+
+# Adding up all six test values (the three that could be made before using only + and * plus the new three that can now be made by also using ||) produces the new total calibration result of 11387.
+
+# Using your new knowledge of elephant hiding spots, determine which equations could possibly be true. What is their total calibration result?
+
 
 import sys
 
@@ -54,25 +70,38 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
-def test_calibration(calibration):
+def concatenate(a, b):
+    return int(str(a) + str(b))
+
+def test_calibration(calibration, operators):
     test_value, operands = calibration
     values = [operands[0]]
     for operand in operands[1:]:
         next_values = []
         for value in values:
-            for fn in [add, multiply]:
-                next_values.append(fn(value, operand))
+            for operator in operators:
+                next_values.append(operator(value, operand))
         values = next_values
     return test_value in values
 
-def part1(calibrations):
+def sum_calibrations(calibrations, operators):
     calibration_sum = 0
     for calibration in calibrations:
-        if test_calibration(calibration):
+        if test_calibration(calibration, operators):
             calibration_sum += calibration[0]
-    
-    print(f'part 1: {calibration_sum}')
+    return calibration_sum
+
+def part1(calibrations):    
+    print(f'part 1: {sum_calibrations(calibrations, [add, multiply])}')
+
+def part2(calibrations):
+    calibration_sum = sum_calibrations(
+        calibrations,
+        [add, multiply, concatenate]
+    )
+    print(f'part 2: {calibration_sum}')
 
 if __name__ == '__main__':
     calibrations = get_calibrations()
     part1(calibrations)
+    part2(calibrations)
