3feb1d541c67a0430922d946082a5a4a9f58c333
Frank Li <li.26.frank@gmail.com>
2024-12-10 22:02:29 -0500

    2024 day 10 part 2

--- 2024/day10/hoof_it.py
+++ 2024/day10/hoof_it.py
@@ -66,6 +66,64 @@
 
 # The reindeer gleefully carries over a protractor and adds it to the pile. What is the sum of the scores of all trailheads on your topographic map?
 
+# --- Part Two ---
+
+# The reindeer spends a few minutes reviewing your hiking trail map before realizing something, disappearing for a few minutes, and finally returning with yet another slightly-charred piece of paper.
+
+# The paper describes a second way to measure a trailhead called its rating. A trailhead's rating is the number of distinct hiking trails which begin at that trailhead. For example:
+
+# .....0.
+# ..4321.
+# ..5..2.
+# ..6543.
+# ..7..4.
+# ..8765.
+# ..9....
+
+# The above map has a single trailhead; its rating is 3 because there are exactly three distinct hiking trails which begin at that position:
+
+# .....0.   .....0.   .....0.
+# ..4321.   .....1.   .....1.
+# ..5....   .....2.   .....2.
+# ..6....   ..6543.   .....3.
+# ..7....   ..7....   .....4.
+# ..8....   ..8....   ..8765.
+# ..9....   ..9....   ..9....
+
+# Here is a map containing a single trailhead with rating 13:
+
+# ..90..9
+# ...1.98
+# ...2..7
+# 6543456
+# 765.987
+# 876....
+# 987....
+
+# This map contains a single trailhead with rating 227 (because there are 121 distinct hiking trails that lead to the 9 on the right edge and 106 that lead to the 9 on the bottom edge):
+
+# 012345
+# 123456
+# 234567
+# 345678
+# 4.6789
+# 56789.
+
+# Here's the larger example from before:
+
+# 89010123
+# 78121874
+# 87430965
+# 96549874
+# 45678903
+# 32019012
+# 01329801
+# 10456732
+
+# Considering its trailheads in reading order, they have ratings of 20, 24, 10, 4, 1, 4, 5, 8, and 5. The sum of all trailhead ratings in this larger example topographic map is 81.
+
+# You're not sure how, but the reindeer seems to have crafted some tiny flags out of toothpicks and bits of paper and is using them to mark trailheads on your topographic map. What is the sum of the ratings of all trailheads?
+
 
 import sys
 
@@ -99,15 +157,52 @@ def calculate_trailhead_score(topographic_map, start_x, start_y):
                 stack.append((x2, y2))
     return len(summits)
 
+def calculate_trailhead_rating(topographic_map, start_x, start_y):
+    rating = 0
+    stack = [(start_x, start_y)]
+    while stack:
+        x, y = stack.pop()
+
+        if topographic_map[x][y] == 9:
+            rating += 1
+            continue
+
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            x2 = x + dx
+            y2 = y + dy
+            if (
+                0 <= x2 < len(topographic_map)
+                and 0 <= y2 < len(topographic_map[x2])
+                and topographic_map[x2][y2] == topographic_map[x][y] + 1
+            ):
+                stack.append((x2, y2))
+    return rating
+
 def part1(topographic_map):
     trailhead_score_sum = 0
     for i in range(len(topographic_map)):
         for j in range(len(topographic_map[i])):
             if topographic_map[i][j] == 0:
-                trailhead_score_sum += calculate_trailhead_score(topographic_map, i, j)
+                trailhead_score = calculate_trailhead_score(
+                    topographic_map, i, j
+                )
+                trailhead_score_sum += trailhead_score
     
     print(f'part 1: {trailhead_score_sum}')
+
+def part2(topographic_map):
+    trailhead_rating_sum = 0
+    for i in range(len(topographic_map)):
+        for j in range(len(topographic_map[i])):
+            if topographic_map[i][j] == 0:
+                trailhead_rating = calculate_trailhead_rating(
+                    topographic_map, i, j
+                )
+                trailhead_rating_sum += trailhead_rating
+    
+    print(f'part 2: {trailhead_rating_sum}')
 
 if __name__ == '__main__':
     topographic_map = get_topographic_map()
     part1(topographic_map)
+    part2(topographic_map)
