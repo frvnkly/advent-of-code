23302e8b2aa6dd13680e4670146d050bed373598
Frank Li <li.26.frank@gmail.com>
2024-12-10 21:39:48 -0500

    2024 day 9 part 2

--- 2024/day9/disk_fragmenter.py
+++ 2024/day9/disk_fragmenter.py
@@ -10,17 +10,17 @@
 
 # The disk map uses a dense format to represent the layout of files and free space on the disk. The digits alternate between indicating the length of a file and the length of free space.
 
-# So, a disk map like 12345 would represent a one-block file, two blocks of free space, a three-block file, four blocks of free space, and then a five-block file. A disk map like 90909 would represent three nine-block files in a row (with no free space between them).
+# So, a disk map like 12345 would represent a one-block file, two disk_map of free space, a three-block file, four disk_map of free space, and then a five-block file. A disk map like 90909 would represent three nine-block files in a row (with no free space between them).
 
-# Each file on disk also has an ID number based on the order of the files as they appear before they are rearranged, starting with ID 0. So, the disk map 12345 has three files: a one-block file with ID 0, a three-block file with ID 1, and a five-block file with ID 2. Using one character for each block where digits are the file ID and . is free space, the disk map 12345 represents these individual blocks:
+# Each file on disk also has an ID number based on the order of the files as they appear before they are rearranged, starting with ID 0. So, the disk map 12345 has three files: a one-block file with ID 0, a three-block file with ID 1, and a five-block file with ID 2. Using one character for each block where digits are the file ID and . is free space, the disk map 12345 represents these individual disk_map:
 
 # 0..111....22222
 
-# The first example above, 2333133121414131402, represents these individual blocks:
+# The first example above, 2333133121414131402, represents these individual disk_map:
 
 # 00...111...2...333.44.5555.6666.777.888899
 
-# The amphipod would like to move file blocks one at a time from the end of the disk to the leftmost free space block (until there are no gaps remaining between file blocks). For the disk map 12345, the process looks like this:
+# The amphipod would like to move file disk_map one at a time from the end of the disk to the leftmost free space block (until there are no gaps remaining between file disk_map). For the disk map 12345, the process looks like this:
 
 # 0..111....22222
 # 02.111....2222.
@@ -45,11 +45,31 @@
 # 00998111888277733364465555.66.............
 # 0099811188827773336446555566..............
 
-# The final step of this file-compacting process is to update the filesystem checksum. To calculate the checksum, add up the result of multiplying each of these blocks' position with the file ID number it contains. The leftmost block is in position 0. If a block contains free space, skip it instead.
+# The final step of this file-compacting process is to update the filesystem checksum. To calculate the checksum, add up the result of multiplying each of these disk_map' position with the file ID number it contains. The leftmost block is in position 0. If a block contains free space, skip it instead.
 
-# Continuing the first example, the first few blocks' position multiplied by its file ID number are 0 * 0 = 0, 1 * 0 = 0, 2 * 9 = 18, 3 * 9 = 27, 4 * 8 = 32, and so on. In this example, the checksum is the sum of these, 1928.
+# Continuing the first example, the first few disk_map' position multiplied by its file ID number are 0 * 0 = 0, 1 * 0 = 0, 2 * 9 = 18, 3 * 9 = 27, 4 * 8 = 32, and so on. In this example, the checksum is the sum of these, 1928.
 
 # Compact the amphipod's hard drive using the process he requested. What is the resulting filesystem checksum? (Be careful copy/pasting the input for this puzzle; it is a single, very long line.)
+
+# --- Part Two ---
+
+# Upon completion, two things immediately become clear. First, the disk definitely has a lot more contiguous free space, just like the amphipod hoped. Second, the computer is running much more slowly! Maybe introducing all of that file system fragmentation was a bad idea?
+
+# The eager amphipod already has a new plan: rather than move individual blocks, he'd like to try compacting the files on his disk by moving whole files instead.
+
+# This time, attempt to move whole files to the leftmost span of free space blocks that could fit the file. Attempt to move each file exactly once in order of decreasing file ID number starting with the file with the highest file ID number. If there is no span of free space to the left of a file that is large enough to fit the file, the file does not move.
+
+# The first example from above now proceeds differently:
+
+# 00...111...2...333.44.5555.6666.777.888899
+# 0099.111...2...333.44.5555.6666.777.8888..
+# 0099.1117772...333.44.5555.6666.....8888..
+# 0099.111777244.333....5555.6666.....8888..
+# 00992111777.44.333....5555.6666.....8888..
+
+# The process of updating the filesystem checksum is the same; now, this example's checksum would be 2858.
+
+# Start over, now compacting the amphipod's hard drive using this new method instead. What is the resulting filesystem checksum?
 
 
 from collections import deque
@@ -58,43 +78,84 @@ import sys
 sys.path.append('.')
 from util import get_input
 
-def part1(disk_map):
-    blocks = deque()
+def get_disk_map():
+    s = get_input('2024/day9/input.txt')[0]
+    disk_map = deque()
     file_id = 0
-    for i in range(0, len(disk_map), 2):
-        blocks.append([file_id, int(disk_map[i])])
+    for i in range(0, len(s), 2):
+        disk_map.append([file_id, int(s[i])])
         file_id += 1
 
-        if i + 1 < len(disk_map) and disk_map[i+1] != '0':
-            blocks.append([None, int(disk_map[i+1])])
-    while blocks[-1][0] is None:
-        blocks.pop()
+        if i + 1 < len(s) and s[i+1] != '0':
+            disk_map.append([None, int(s[i+1])])
+    while disk_map[-1][0] is None:
+        disk_map.pop()
+    return disk_map
 
-    compressed = []
-    while blocks:
-        if not blocks[0][0] is None:
-            compressed.append(blocks.popleft())
-            continue
-        
-        num_blocks = min(blocks[0][1], blocks[-1][1])
-        compressed.append([blocks[-1][0], num_blocks])
-        blocks[0][1] -= num_blocks
-        if blocks[0][1] <= 0: blocks.popleft()
-        blocks[-1][1] -= num_blocks
-        if blocks[-1][1] <= 0: blocks.pop()
-
-        while blocks and blocks[-1][0] is None:
-            blocks.pop()
-
+def calculate_checksum(disk_map):
     checksum = 0
     position = 0
-    for file_id, num_blocks in compressed:
-        for n in range(num_blocks):
+    for file_id, num_disk_map in disk_map:
+        for n in range(num_disk_map):
+            if file_id is None: continue
             checksum += file_id * (position + n)
-        position += num_blocks
+        position += num_disk_map
+    return checksum
 
-    print(f'part 1: {checksum}')
+def part1():
+    disk_map = get_disk_map()
+
+    compressed = []
+    while disk_map:
+        if not disk_map[0][0] is None:
+            compressed.append(disk_map.popleft())
+            continue
+        
+        num_disk_map = min(disk_map[0][1], disk_map[-1][1])
+        compressed.append([disk_map[-1][0], num_disk_map])
+        disk_map[0][1] -= num_disk_map
+        if disk_map[0][1] <= 0: disk_map.popleft()
+        disk_map[-1][1] -= num_disk_map
+        if disk_map[-1][1] <= 0: disk_map.pop()
+
+        while disk_map and disk_map[-1][0] is None:
+            disk_map.pop()
+
+    print(f'part 1: {calculate_checksum(compressed)}')
+
+def part2():
+    disk_map = get_disk_map()
+
+    compressed = []
+    while disk_map:
+        next_span = disk_map.popleft()
+
+        if not next_span[0] is None:
+            compressed.append(next_span)
+            continue
+        
+        while next_span[1] > 0:
+            while disk_map and disk_map[-1][0] is None:
+                disk_map.pop()
+
+            shifted = False
+            for i in range(len(disk_map) - 1, -1, -1):
+                if disk_map[i][0] is None:
+                    continue
+
+                if disk_map[i][1] <= next_span[1]:
+                    compressed.append([disk_map[i][0], disk_map[i][1]])
+                    next_span[1] -= disk_map[i][1]
+                    disk_map[i][0] = None
+                    shifted = True
+                    break
+                    
+            if not shifted:
+                compressed.append(next_span)
+                break
+
+    print(f'part 2: {calculate_checksum(compressed)}')
 
 if __name__ == '__main__':
-    disk_map = get_input('2024/day9/input.txt')[0]
-    part1(disk_map)
+    part1()
+    part2()
